922c950cb6c52bbcdc044d8d1c3923e3b2d61b50
Diego Concolato <72687738+thisisdconc@users.noreply.github.com>
2024-05-02 10:07:04 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# khaos-rp-site
+# KHAOS RP - Official Site
+
+IT - Sito ufficiale di KHAOS RP sviluppato da Qspace Corporation
+
+EN - Official KHAOS RP website developed by Qspace Corporation
--- js/script.js
+++ js/script.js
@@ -152,48 +152,50 @@ function page4_gsap() {
     })
 
 
-   
+
 }
 
-function textSplit(){
-    var tl = gsap.timeline({scrollTrigger:{
-        trigger:".elemb-p4",
-        
-        // markers:true,
-        start:"50% 50%",
-        end:"350% 50%",
-        scrub:2,
-        pin:true
-    }});
+function textSplit() {
+    var tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".elemb-p4",
+
+            // markers:true,
+            start: "50% 50%",
+            end: "350% 50%",
+            scrub: 2,
+            pin: true
+        }
+    });
     tl
-    .to("#center",{
-       height: "100vh",
-    },'a')
-    .to("#top",{
-        top: "-50%",
-     },'a')
-     .to("#bottom",{
-        bottom: "-50%",
-     },'a')
-    .to("#top-h1",{
-        bottom: "10%"
-     },'a')
-     .to("#bottom-h1",{
-        bottom: "-30%"
-     },'a')
-    .to("#center-h1",{
-       top: "-30%"
-    },'a')
-    .to(".content",{
-       delay: -0.2,
-       marginTop: "0%"
-    })
-    .to(".content",{
-        opacity: 0,
-        duration:1
-     })
-    
-    
+        .to("#center", {
+            height: "100vh",
+        }, 'a')
+        .to("#top", {
+            top: "-50%",
+        }, 'a')
+        .to("#bottom", {
+            bottom: "-50%",
+        }, 'a')
+        .to("#top-h1", {
+            bottom: "10%"
+        }, 'a')
+        .to("#bottom-h1", {
+            bottom: "-30%"
+        }, 'a')
+        .to("#center-h1", {
+            top: "-30%"
+        }, 'a')
+        .to(".content", {
+            delay: -0.2,
+            marginTop: "0%"
+        })
+        .to(".content", {
+            opacity: 0,
+            duration: 1
+        })
+
+
 }
 
 
